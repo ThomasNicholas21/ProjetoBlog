325b5375d349429f03a2739e8956de730ebdb399
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-27 10:23:48 -0300

    feat: configurando media e static para que o django possa servir esses arquivos em produção

--- app/project/urls.py
+++ app/project/urls.py
@@ -1,22 +1,18 @@
 """
-URL configuration for project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+https://docs.djangoproject.com/en/4.2/topics/http/urls/
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    # permite que o dev tenha acesso aos arquivos enviados pelo user
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # permite que o dev tenha acesso aos arquivos estáticos
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
