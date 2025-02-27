d5f476c1be7023d411d30b699b8189bbadea7886
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-27 10:02:16 -0300

    feat: configurando variável de ambiente SECRET_KEY

--- app/project/settings.py
+++ app/project/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q!6y&^n$7+rw4hplq()a(4yuu1re^2t!9(x2pki81-ewti=d5$'
+# Pega variável de ambiente criáda, e caso não encontre ela retorna
+# change-me
+SECRET_KEY = os.getenv('SECRET_KEY', 'change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Busca variáveis de ambiente para retornar um booleano
