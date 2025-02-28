8ba9cc19f9cd0331e9ede85b3af6112f035f03ca
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-28 13:24:11 -0300

    feat: configurando novo model na aba admin

--- app/site_setup/admin.py
+++ app/site_setup/admin.py
@@ -4,7 +4,15 @@ from site_setup import models
 # Register your models here.
 
 @admin.register(models.MenuLink)
-class MenuLink(admin.ModelAdmin):
-    list_display = 'id', 'text', 'url_or_path', 'new_tab'
-    list_display_links = 'id', 'text', 'url_or_path', 'new_tab'
-    search_fields = 'id', 'text', 'url_or_path', 'new_tab'
+class MenuLinkAdmin(admin.ModelAdmin):
+    list_display = 'id', 'text', 'url_or_path', 'new_tab',
+    list_display_links = 'id', 'text', 'url_or_path', 'new_tab',
+    search_fields = 'id', 'text', 'url_or_path', 'new_tab',
+
+
+@admin.register(models.SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = 'title', 'description',
+
+    def has_add_permission(self, request):
+        return not models.SiteSetup.objects.all()[:1].exists()
