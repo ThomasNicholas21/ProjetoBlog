2434986d06daff13c4a2ffe82404531381dd1f26
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-28 11:11:43 -0300

    feat: incluindo url do aplicativo blog

--- app/project/urls.py
+++ app/project/urls.py
@@ -4,9 +4,10 @@ https://docs.djangoproject.com/en/4.2/topics/http/urls/
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
 
