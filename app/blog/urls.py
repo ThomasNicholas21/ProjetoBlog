d6d311a0dd5e88c883794c4dc57cffabbf004218
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-28 11:14:38 -0300

    feat: criando urls de blog e incluindo primeira view

---
+++ app/blog/urls.py
@@ -0,0 +1,12 @@
+"""
+https://docs.djangoproject.com/en/4.2/topics/http/urls/
+"""
+from django.contrib import admin
+from django.urls import path
+from blog import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
