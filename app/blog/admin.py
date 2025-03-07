49f60285f59affce0171f170ed472ce8786b34a3
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-07 13:27:26 -0300

    feat: configurando entidade tag no painél administrativo do django

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from blog import models
 # Register your models here.
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'slug', 
+    list_display_links = 'name',
+    search_fields = 'id', 'name', 'slug',
+    list_per_page = 10
+    ordering = '-id',
+    prepopulated_fields = {
+        'slug': ('name',),
+    }
