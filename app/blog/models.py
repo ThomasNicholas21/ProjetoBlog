71ff597e8fd89d5d1579fd8cd2d10d876d5b84af
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-07 13:39:20 -0300

    feat: criando entidade model

--- app/blog/models.py
+++ app/blog/models.py
@@ -24,3 +24,23 @@ class Tag(models.Model):
         return self.name
 
 
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        unique=True, default=None,
+        null=True, blank=True,
+        max_length=255
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.name)
+        
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
