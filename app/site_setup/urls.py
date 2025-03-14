d1cd29da72d8b909d74b5b42801da52136c89f70
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-13 21:48:10 -0300

    fix: retirando url errada

--- app/site_setup/urls.py
+++ app/site_setup/urls.py
@@ -2,10 +2,8 @@
 https://docs.djangoproject.com/en/4.2/topics/http/urls/
 """
 from django.urls import path
-from blog import views
 
 app_name = 'site_setup'
 
 urlpatterns = [
-    path('', views.index, name='index'),
 ]
